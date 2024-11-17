ecdf833d0d96c17b5b844082da8410ce82c312aa
arthur5040 <arthursaubara@gmail.com>
2024-11-16 21:40:00 -0300

    ha

---
+++ Aula7-Objects&KYThis/script.js
@@ -0,0 +1,8 @@
+// para criar um objeto, escreva como se fosse uma função, mas o título será uma "classe" q posssui elementos dentro dela. por ex:
+
+const usuario = {
+    nome:"Joao",
+    idade: 20,
+    cidade: "São Paulo",
+}
+console.log(usuario.idade)
